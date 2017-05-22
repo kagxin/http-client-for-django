d73c8d68006763f5afc21d34117ef422c7d028d2
康鑫 <kangxin@192.168.10.3>
2017-05-22 15:58:41 +0800

    login

--- client.py
+++ client.py
@@ -1,54 +1,13 @@
+#!/usr/bin/python
 
-# -*- coding: utf8 -*-
 import cookielib
 import json
 import urllib
 import urllib2
 import logging
 
-REMOTE_HOST = 'localhost'
+REMOTE_HOST = '192.168.1.244'
 REMOTE_PORT = 80
-
-def _use_cookie():
-    cj = cookielib.CookieJar()
-    handler = urllib2.HTTPCookieProcessor(cj)
-    opener = urllib2.build_opener(handler)
-    urllib2.install_opener(opener)
-    return cj
-
-def remote_login(username, password, **kwargs):
-    cj = _use_cookie()
-    remote_host = kwargs.get('host', REMOTE_HOST)
-    remote_port = kwargs.get('port', REMOTE_PORT)
-    remote_uri = kwargs.get('uri', 'mobile/login/')
-    url = "http://%s:%d/%s" % (remote_host, remote_port, remote_uri)
-
-    urllib2.urlopen(url)
-    csrftoken = ''
-    for cookie in cj:
-        if cookie.name == 'csrftoken':
-            csrftoken = cookie.value
-
-    data = {}
-    data['username'] = username
-    data['password'] = password
-    data['csrfmiddlewaretoken'] = csrftoken
-
-    res = urllib2.urlopen(url, urllib.urlencode(data))
-    remote_session = ''
-    for cookie in cj:
-        if cookie.name == 'sessionid':
-            remote_session = cookie.value
-    jsonhtml = res.read()
-    resobj = json.loads(jsonhtml)
-    if int(resobj['status']) == 0:
-        cj.clear()
-        return None
-
-    resobj['remote_session'] = remote_session
-    # cj.clear() #不清除session
-    return resobj
-
 # res = remote_login(username='kx', password='2012', uri='mobile/login/')
 # print(res)
 #
@@ -61,16 +20,65 @@ def remote_login(username, password, **kwargs):
 # data.update(tmp)
 # res = urllib2.urlopen("http://localhost/mobile/register/", urllib.urlencode(data))
 # print(res.read())
+ 
 
-def test_update_user_info():
-    username = 'kx'
-    phone = '18211710965'
-    email = 'k123@123.com'
-    gender = "嘉定区"
-    address = "菊园"
-    position = '老师'
-    company = '赢谊'
-    f = read('D:/test.jpg')
-    jpg = f.read()
-    head_protrait = jpg
-    bg_image = jpg
+class DjangoClient(object):
+    def __init__(self, username, password):
+        self._login(username, password)
+        self.error_cnt = 0
+        
+    def _use_cookie(self):
+        cj = cookielib.CookieJar()
+        handler = urllib2.HTTPCookieProcessor(cj)
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
+        return cj
+
+    def _login(self, username, password, **kwargs):
+        cj = self._use_cookie()
+        remote_host = kwargs.get('host', REMOTE_HOST)
+        remote_port = kwargs.get('port', REMOTE_PORT)
+        remote_uri = kwargs.get('uri', 'mobile/login/')
+        url = "http://%s:%d/%s" % (remote_host, remote_port, remote_uri)
+
+        urllib2.urlopen(url)
+        csrftoken = ''
+        for cookie in cj:
+            if cookie.name == 'csrftoken':
+                csrftoken = cookie.value
+
+        data = {}
+        data['username'] = username
+        data['password'] = password
+        data['csrfmiddlewaretoken'] = csrftoken
+
+        res = urllib2.urlopen(url, urllib.urlencode(data))
+        remote_session = ''
+        for cookie in cj:
+            if cookie.name == 'sessionid':
+                remote_session = cookie.value
+                jsonhtml = res.read()
+                resobj = json.loads(jsonhtml)
+        if int(resobj['status']) == 0:
+            cj.clear()
+            return None
+
+        resobj['remote_session'] = remote_session
+        
+        return resobj
+
+    def get(self, url):
+        url = "http://%s:%d"%(REMOTE_HOST,REMOTE_PORT) + url
+        res = urllib2.urlopen(url)
+        if res.code != 200:
+            self.error_cnt = self.error_cnt + 1
+
+        return res
+
+    def post(self, url, para):
+        pass
+    
+client = DjangoClient('kx', '2017')
+
+res = client.get("/mobile/")
+print(res.read())
